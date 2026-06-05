0683d80f7a352f3a1c8f29fd61fd810d743446dd
peetlamahesh <peetlamahesh81@gmail.com>
2026-06-05 14:44:54 +0530

    changed to hashrouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App.jsx';
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "./context/themeContext";
 
-import { ThemeProvider } from './context/themeContext';
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/taskmasterapp">
+    <HashRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
